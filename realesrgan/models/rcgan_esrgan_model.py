e970ea9d7c996bceac7dd11d81a46d311bc7ba2c
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-06-20 17:08:15 -0400

    Changed model to be small and use wgan

--- realesrgan/models/rcgan_esrgan_model.py
+++ realesrgan/models/rcgan_esrgan_model.py
@@ -325,7 +325,24 @@ class rcGANESRGAN(SRGANModel):
         loss_dict['out_d_fake'] = torch.mean(fake_d_pred.detach())
         l_d_fake.backward()
         # GP
-        l_d_gp = 10 * gradient_penalty_loss(self.net_d, gan_gt, self.output[0, :, :, :, :])
+        batch_size = real_data.size(0)
+        alpha = torch.rand(batch_size, 1, 1, 1).to(gan_gt.device)
+
+        # interpolate between real_data and fake_data
+        interpolates = alpha * gan_gt + (1. - alpha) * self.output[0, :, :, :, :].detach().clone()
+        interpolates = autograd.Variable(interpolates, requires_grad=True)
+
+        disc_interpolates = discriminator(interpolates)
+        gradients = autograd.grad(
+            outputs=disc_interpolates,
+            inputs=interpolates,
+            grad_outputs=torch.ones_like(disc_interpolates),
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True)[0]
+
+        gradients_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        l_d_gp = 10 * gradients_penalty
         loss_dict['l_d_gp'] = l_d_gp
         l_d_gp.backward()
 
