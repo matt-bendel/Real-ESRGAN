edb8f76cd4d00080930221c19a40b3d72c3c911c
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-07-04 15:20:20 -0400

    Fixed resume

--- realesrgan/models/rcgan_esrgan_model.py
+++ realesrgan/models/rcgan_esrgan_model.py
@@ -289,10 +289,10 @@ class rcGANESRGAN(SRGANModel):
         loss_dict = OrderedDict()
         if (current_iter % self.net_d_iters == 0 and current_iter > self.net_d_init_iters):
             # pixel loss
-            if self.cri_pix:
-                l_g_pix = self.cri_pix(self.output, l1_gt)
-                l_g_total += l_g_pix
-                loss_dict['l_g_pix'] = l_g_pix
+            # if self.cri_pix:
+            #     l_g_pix = self.cri_pix(self.output, l1_gt)
+            #     l_g_total += l_g_pix
+            #     loss_dict['l_g_pix'] = l_g_pix
 
             if self.cri_perceptual:
                 l_g_percept = 0
